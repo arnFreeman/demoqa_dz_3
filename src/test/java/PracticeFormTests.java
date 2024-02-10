80caf0ca8bcbd7c78d32431dd0bfaf223b481977
RenatAbdullin <arn_freeman@mail.ru>
2024-02-10 23:34:33 +0500

    added Resources Directory

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -29,7 +29,7 @@ public class PracticeFormTests {
         $(".react-datepicker__month").$(byText("28")).click();
         $("#subjectsInput").setValue("Commerce").pressEnter();
         $("#hobbiesWrapper").$(byText("Reading")).click();
-        $("#uploadPicture").uploadFromClasspath("1221.jpg");
+        $("#uploadPicture").uploadFromClasspath("1223.jpg");
         $("#currentAddress").setValue("Baikonur Cosmodrome");
         $("#state").click();
         $("#state").$(byText("Uttar Pradesh")).click();
@@ -44,7 +44,7 @@ public class PracticeFormTests {
                 text("28 April,1900"),
                 text("Commerce"),
                 text("Reading"),
-                text("1221.jpg"),
+                text("1223.jpg"),
                 text("Baikonur Cosmodrome"),
                 text("Uttar Pradesh Lucknow"));
 
